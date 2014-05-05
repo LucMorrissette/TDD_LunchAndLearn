1afb867f9dcae16588f870924eb6ceba89c06892
LucMorrissette <lucmorrissette@hotmail.com>
2014-05-04 21:22:38 -0400

    another tweak

--- FizzBuzzWebForms/Behaviors/FizzBuzzInterpreter.cs
+++ FizzBuzzWebForms/Behaviors/FizzBuzzInterpreter.cs
@@ -9,7 +9,7 @@ namespace FizzBuzzWebForms.Behaviors
             if(input < 1 || input > 100)
                 throw new ArgumentException("Input is out of range.");
 
-            if (input % 15 == 0)
+            if (input % 3 == 0 && input % 5 == 0)
                 return "FizzBuzz";
             
             if (input % 3==0)
