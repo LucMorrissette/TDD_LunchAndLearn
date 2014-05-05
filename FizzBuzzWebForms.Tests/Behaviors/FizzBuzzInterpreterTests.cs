4d470dca49185daec0a9460a36c93b5286081b68
LucMorrissette <lucmorrissette@hotmail.com>
2014-05-04 21:39:33 -0400

    Refactored unit test class

--- FizzBuzzWebForms.Tests/Behaviors/FizzBuzzInterpreterTests.cs
+++ FizzBuzzWebForms.Tests/Behaviors/FizzBuzzInterpreterTests.cs
@@ -7,26 +7,32 @@ namespace FizzBuzzWebForms.Tests.Behaviors
     [TestFixture]
     public class FizzBuzzInterpreterTests
     {
+        private FizzBuzzInterpreter interpreter;
+        
+        [SetUp]
+        public void Init()
+        {
+            interpreter = new FizzBuzzInterpreter();
+        }
+        
+        
         [Test]
         [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Input is out of range.")]
         public void Given101ExpectException()
         {
-            var interpreter = new FizzBuzzInterpreter();
-            var actual = interpreter.Interpret(101);
+            interpreter.Interpret(101);
         }
 
         [Test]
         [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Input is out of range.")]
         public void GivenZeroExpectException()
         {
-            var interpreter = new FizzBuzzInterpreter();
-            var actual = interpreter.Interpret(0);
+            interpreter.Interpret(0);
         }
 
         [Test]
         public void Given1Expect1()
         {
-            var interpreter = new FizzBuzzInterpreter();
             var expected = "1";
             var actual = interpreter.Interpret(1);
             Assert.AreEqual(expected, actual);
@@ -35,39 +41,52 @@ namespace FizzBuzzWebForms.Tests.Behaviors
         [Test]
         public void Given2Expect2()
         {
-            var interpreter = new FizzBuzzInterpreter();
             var expected = "2";
             var actual = interpreter.Interpret(2);
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void Given3ExpectFizz()
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(12)]
+        [TestCase(21)]
+        [TestCase(33)]
+        [TestCase(63)]
+        public void GivenDivisibleby3AndNot5ExpectFizz(int input)
         {
-            var interpreter = new FizzBuzzInterpreter();
             var expected = "Fizz";
-            var actual = interpreter.Interpret(3);
+            var actual = interpreter.Interpret(input);
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void Given5ExpectBuzz()
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(25)]
+        [TestCase(50)]
+        [TestCase(55)]
+        [TestCase(100)]
+        public void GivenDivisibleBy5AndNot3ExpectBuzz(int input)
         {
-            var interpreter = new FizzBuzzInterpreter();
             var expected = "Buzz";
-            var actual = interpreter.Interpret(5);
+            var actual = interpreter.Interpret(input);
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void Given15ExpectFizzBuzz()
+        [TestCase(01 * 03 * 05)]
+        [TestCase(02 * 03 * 05)]
+        [TestCase(03 * 03 * 05)]
+        [TestCase(04 * 03 * 05)]
+        [TestCase(05 * 03 * 05)]
+        public void GiveDivisibleByBoth5and3ExpectFizzBuzz(int input)
         {
-            var interpreter = new FizzBuzzInterpreter();
             var expected = "FizzBuzz";
-            var actual = interpreter.Interpret(15);
+            var actual = interpreter.Interpret(input);
             Assert.AreEqual(expected, actual);
         }
 
+       
 
 
     }
